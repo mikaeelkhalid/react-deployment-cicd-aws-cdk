fd540c11cdf3bac3b402f5bdf007b5cbbfaf2f1d
mikaeel khalid <mikaeelkhalid@icloud.com>
2023-11-01 05:24:51 +0100

    add web bucket

--- stacks/react-deployment-cicd-stack.ts
+++ stacks/react-deployment-cicd-stack.ts
@@ -1,8 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket, BucketAccessControl, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 interface ReactDeploymentCICDStackProps extends StackProps {
-  isDeploy: boolean;
   environmentType: string;
   branch: string;
   pipelineName: string;
@@ -17,8 +17,26 @@ interface ReactDeploymentCICDStackProps extends StackProps {
 }
 
 export class ReactDeploymentCICDStack extends Stack {
-  constructor(scope: Construct, id: string, props?: ReactDeploymentCICDStackProps) {
+  constructor(scope: Construct, id: string, props: ReactDeploymentCICDStackProps) {
     super(scope, id, props);
+
+    const webBucket = this._createWebBucket(props);
+  }
+
+  private _createWebBucket(props: ReactDeploymentCICDStackProps) {
+    const { bucketName, indexFile, errorFile, publicAccess } = props;
+
+    const webBucket = new Bucket(this, bucketName, {
+      websiteIndexDocument: indexFile,
+      websiteErrorDocument: errorFile,
+      publicReadAccess: publicAccess,
+      removalPolicy: RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    return webBucket;
   }
 }
 
