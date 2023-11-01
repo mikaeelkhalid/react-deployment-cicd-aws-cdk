45e9149937a7dcd0600f807ba02d5c02c366bc80
mikaeel khalid <mikaeelkhalid@icloud.com>
2023-11-01 05:06:53 +0100

    add props interface

--- stacks/react-deployment-cicd-stack.ts
+++ stacks/react-deployment-cicd-stack.ts
@@ -1,8 +1,22 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+interface ReactDeploymentCICDStackProps extends StackProps {
+  isDeploy: boolean;
+  environmentType: string;
+  branch: string;
+  pipelineName: string;
+  bucketName: string;
+  pipelineBucket: string;
+  publicAccess: boolean;
+  indexFile: string;
+  errorFile: string;
+  githubRepoOwner: string;
+  githubRepoName: string;
+}
+
 export class ReactDeploymentCICDStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: ReactDeploymentCICDStackProps) {
     super(scope, id, props);
   }
 }
